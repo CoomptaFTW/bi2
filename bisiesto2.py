08a5449416bb6822ae605824a1576de1577d918f
ism41670634 <ism41670634@a11.informatica.escoladeltreball.org>
2018-01-25 13:49:34 +0100

    first commit

---
+++ bisiesto2.py
@@ -0,0 +1,10 @@
+#coding: utf8
+def esBisiesto (anyo):
+	if (anyo1%4==0 and anio1%100!==0 or anio1%400==0:)
+		print "es bisiesto"
+	else:
+		print "no es bisiesto"
+
+esBisiesto(2100)
+esBisiesto(2300)
+esBisiesto(1999)
